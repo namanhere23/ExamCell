35be51dcc60b059ff2bcb67cc5f421e635eff521
ThunderBlaze <darkthunderstormshiva@gmail.com>
2025-05-01 05:45:25 +0530

    Fixed All Errors

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 
