e0361e022f66a5b30a7420a9836fd4c6bbea3584
diyaanna06 <diyavarghese20june@gmail.com>
2025-05-01 17:28:22 +0530

    History Logs

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
+import History from "./pages/History";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/History" element={<History/>} />
       </Routes>
     </BrowserRouter>
   );
